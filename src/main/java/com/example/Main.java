a589aa3012a88bf4d29fe1a70b940e2ee006eb27
iconbaypark2900 <jonaston015@gmail.com>
2023-06-01 13:55:35 -0400

    getting closer

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         new SandwichOrderingSystem().placeOrder();
