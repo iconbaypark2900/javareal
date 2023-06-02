34fe5c06e449568488c43ca500eefdc35a2ec40c
iconbaypark2900 <jonaston015@gmail.com>
2023-06-01 20:04:08 -0400

    done

--- src/main/java/com/example/Didit.java
+++ src/main/java/com/example/Didit.java
@@ -6,14 +6,17 @@ public class Didit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Order VIP = new Order(1);
+        SandwichOrderingSystem sos = new SandwichOrderingSystem();
+        boolean continueRunning = true;
 
         System.out.println("Welcome to BigDaddy's Diner");
         while(true) {
             System.out.println("PLEASE SELECT A OPTION");
             System.out.println("Press 1 for New Order");
-            System.out.println("Press 2 to Exit");
+            System.out.println("Press 2 for  Big Daddy's Diner Recommender");
+            System.out.println("Press 3 to Exit");
             int ans = scan.nextInt();
-            while(ans == 1){
+            while (ans == 1) {
                 System.out.println("PLEASE SELECT A OPTION");
                 System.out.println("Press 1 -> ADD SANDWICH");
                 System.out.println("Press 2 -> ADD DRINK");
@@ -21,18 +24,18 @@ public class Didit {
                 System.out.println("Press 4 -> TO CHECKOUT");
                 int ans1 = scan.nextInt();
                 //Constraint to prevent the user from entering a invalid number.
-                while(ans1 <= 0 && ans1 >=4) {
+                while (ans1 <= 0 && ans1 >= 4) {
                     System.out.println("Please enter a valid value.");
                     ans1 = scan.nextInt();
                 }
-                if(ans1 == 1){
+                if (ans1 == 1) {
                     System.out.println("BREAD TYPES - (WHEAT,WHITE,WRAP,RYE)");
                     System.out.println("Which breadtype would you like ?");
                     scan.nextLine();
                     String breadtype = scan.nextLine();
                     //Constraint to prevent the user from entering an INVALID Input
-                    while(!(breadtype.equalsIgnoreCase("WHEAT") || breadtype.equalsIgnoreCase("WHITE")
-                            || breadtype.equalsIgnoreCase("WRAP") || breadtype.equalsIgnoreCase("RYE"))){
+                    while (!(breadtype.equalsIgnoreCase("WHEAT") || breadtype.equalsIgnoreCase("WHITE")
+                            || breadtype.equalsIgnoreCase("WRAP") || breadtype.equalsIgnoreCase("RYE"))) {
                         System.out.println("PLEASE ENTER A VALID BREADTYPE!!");
                         breadtype = scan.nextLine();
                     }
@@ -40,7 +43,7 @@ public class Didit {
                     System.out.println("What size would you like ?");
                     int size = scan.nextInt();
                     //Constraint to prevent the user from entering an INVALID Input
-                    while(!(size == 4 || size == 8 || size == 12)) {
+                    while (!(size == 4 || size == 8 || size == 12)) {
                         System.out.println("Please enter a valid value.");
                         size = scan.nextInt();
                     }
@@ -48,20 +51,19 @@ public class Didit {
                     System.out.println("IF YES ENTER 1 IF NO ENTER 2");
                     int choice = scan.nextInt();
                     //Constraint to prevent the user from entering an INVALID Input
-                    while(!(choice == 1 || choice == 2)) {
+                    while (!(choice == 1 || choice == 2)) {
                         System.out.println("Please enter a valid value.");
                         size = scan.nextInt();
                     }
                     // Boolean Variable capturing whether the sandwich is toasted
                     boolean toast;
-                    if(choice == 1){
+                    if (choice == 1) {
                         toast = true;
-                    }
-                    else{
+                    } else {
                         toast = false;
                     }
                     //Creating the Sandwich
-                    Sandwich CRABBYPATTY = new Sandwich(breadtype,size,toast);
+                    Sandwich CRABBYPATTY = new Sandwich(breadtype, size, toast);
                     System.out.println("MEAT TOPPING OPTIONS - STEAK, HAM, SALAMI, ROAST BEEF, CHICKEN,");
                     System.out.println("ONLY ONE OPTION ALLOWED!!. PLEASE ENTER YOUR CHOICE");
                     scan.nextLine();
@@ -93,46 +95,44 @@ public class Didit {
                         CRABBYPATTY.addExtraCheese(extraTopping);
                     }
                     //This loops through the ArrayList for all Regular Toppings
-                    for(String str : CRABBYPATTY.regToppings()){
-                       System.out.println("Would you like to add " + str + " to your sandwich");
-                       System.out.println("Type 1 for Yes or 2 for No");
-                       int ans56 = scan.nextInt();
-                       if (ans56 == 1){
-                           CRABBYPATTY.addRegularTopping(str);
-                       }
-                   }
+                    for (String str : CRABBYPATTY.regToppings()) {
+                        System.out.println("Would you like to add " + str + " to your sandwich");
+                        System.out.println("Type 1 for Yes or 2 for No");
+                        int ans56 = scan.nextInt();
+                        if (ans56 == 1) {
+                            CRABBYPATTY.addRegularTopping(str);
+                        }
+                    }
 
                     //This loops through the ArrayList for all Sauce Toppings
-                    for(String str : CRABBYPATTY.sToppings()){
+                    for (String str : CRABBYPATTY.sToppings()) {
                         System.out.println("Would you like to add " + str + " to your sandwich");
                         System.out.println("Type 1 for Yes or 2 for No");
                         int ans66 = scan.nextInt();
-                        if (ans66 == 1){
+                        if (ans66 == 1) {
                             CRABBYPATTY.addSauces(str);
                         }
                     }
                     //Adding the Sandwich to the Order
                     VIP.addSandWICH(CRABBYPATTY);
 
-                }
-                else if(ans1 == 2){
+                } else if (ans1 == 2) {
                     System.out.println("DRINK SIZES - SMALL,MEDIUM,LARGE");
                     System.out.println("PLEASE ENTER THE SIZE YOU WOULD LIKE");
                     scan.nextLine();
                     String size = scan.nextLine();
                     //Constraint to prevent the user from entering an INVALID Input
-                    while(!(size.equalsIgnoreCase("SMALL") || size.equalsIgnoreCase("MEDIUM") || size.equalsIgnoreCase("LARGE"))) {
+                    while (!(size.equalsIgnoreCase("SMALL") || size.equalsIgnoreCase("MEDIUM") || size.equalsIgnoreCase("LARGE"))) {
                         System.out.println("Please enter a valid value.");
                         size = scan.nextLine();
                     }
                     System.out.println("PLEASE ENTER A DRINK FLAVOR");
                     String soda = scan.nextLine();
                     //Creating the Drink
-                    Drink StreetVybez = new Drink(size,soda);
+                    Drink StreetVybez = new Drink(size, soda);
                     //Adding the Drink to the Order
                     VIP.addDrinks(StreetVybez);
-                }
-                else if(ans1 == 3){
+                } else if (ans1 == 3) {
                     scan.nextLine();
                     System.out.println("ENTER NAME OF CHIP YOU WOULD LIKE TO PURCHASE");
                     String chipName = scan.nextLine();
@@ -140,18 +140,16 @@ public class Didit {
                     Chip LAYS = new Chip(chipName);
                     //Adding the Drink to the Order
                     VIP.addChips(LAYS);
-                }
-                else if (ans1 == 4) {
+                } else if (ans1 == 4) {
                     VIP.printOrderInfo();
                     System.out.println("IF ORDER IS CORRECT TYPE 1. IF YOU WOULD LIKE TO CANCEL YOUR ORDER TYPE 2");
                     int ans2 = scan.nextInt();
-                    if(ans2 == 1){
+                    if (ans2 == 1) {
                         // This function saves your oder.
                         VIP.saveorder();
                         VIP.deleteorder();
                         break;
-                    }
-                    else{
+                    } else {
                         // This function deletes your order.
                         VIP.deleteorder();
                         break;
@@ -159,19 +157,23 @@ public class Didit {
                 }
 
             }
-            if(ans == 2){
-                System.out.println("THANK YOU HAVE A WONDERFUL DAY");
-               break;
-            }
-            else{
-                //Constraint to ensure the user enter a valid option to start the program
-                while(!(ans == 2 || ans == 1)) {
-                    System.out.println("Please enter a valid value.");
-                    ans = scan.nextInt();
+            while (continueRunning) {
+                if (ans == 2) {
+                    sos.placeOrder();
+                    continueRunning = false;
+                }
+                if (ans == 3) {
+                    System.out.println("THANK YOU HAVE A WONDERFUL DAY");
+                    break;
+                } else {
+                    //Constraint to ensure the user enter a valid option to start the program
+                    while (!(ans == 2 || ans == 1)) {
+                        System.out.println("Please enter a valid value.");
+                        ans = scan.nextInt();
+                    }
                 }
             }
         }
-
 
     }
 }
